e35cd9b8bdd09df32193fff13b5a7401a432e28e
Ranit Mandal <ranitmandalofficial@gmail.com>
2026-08-20 10:36:51 +0530

    ok2

--- Array/Recommended.js
+++ Array/Recommended.js
@@ -102,8 +102,8 @@ console.log(firstRepeated([1, 2, 3, 4, 5])); // Output: -1
 
 const countZeros = (arr) => {
     let count = 0;
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] === 0) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
             count++;
         }
     }
@@ -111,3 +111,99 @@ const countZeros = (arr) => {
 }
 console.log(countZeros([1, 1, 1, 0, 0])); // Output: 2
 console.log(countZeros([1, 1, 1, 1, 1])); // Output: 0
+
+//! Given a sorted array arr[] and an integer x, find the index (0-based) of the largest element in arr[] that is 
+//! less than or equal to x. This element is called the floor of x. If such an element does not exist, return -1.
+//Note: In case of multiple occurrences of floor of x, return the index of the last occurrence
+
+const floorOfX = (arr, x) => {
+    let low = 0;
+    let high = arr.length - 1;
+    let floorIndex = -1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if (arr[mid] <= x) {
+            floorIndex = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return floorIndex;
+};
+console.log(floorOfX([1, 2, 3, 4, 5], 3)); // Output: 2
+console.log(floorOfX([1, 2, 3, 4, 5], 0)); // Output: -1
+
+//! Given an array of integers arr[] that is first strictly increasing and then maybe strictly decreasing, 
+//! find the bitonic point, that is the maximum element in the array. Bitonic Point is a point before which elements are strictly 
+//! increasing and after which elements are strictly decreasing.
+// Note: It is guaranteed that the array contains exactly one bitonic point.
+
+const bitonicPoint = arr => {
+    let bitonicPoint = arr[0]
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < arr[i + 1]) {
+            bitonicPoint = arr[i + 1]
+        }
+    }
+    return bitonicPoint
+}
+console.log(bitonicPoint([1, 3, 6, 8, 4, 2, 1])) // Output: 8
+console.log(bitonicPoint([120, 100, 80, 20, 0])) // Output: 120
+
+//! Given an array arr[], check whether it is sorted in non-decreasing order. Return true if it is sorted otherwise false
+const isSortedAArray = (arr) => {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+};
+console.log(isSortedAArray([1, 2, 3, 4, 5])); // Output: true
+console.log(isSortedAArray([1, 3, 2, 4, 5])); // Output: false
+
+//! Given an array arr[] consisting of only 0's and 1's. Modify the array in-place to segregate 0s onto the left 
+//! side and 1s onto the right side of the array.
+
+// type 1 : 
+const segregateZerosAndOnesType1 = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                [arr[i], arr[j]] = [arr[j], arr[i]]
+            }
+        }
+    }
+
+    return arr
+}
+console.log(segregateZerosAndOnesType1([0, 1, 0, 1, 0])); // Output: [0, 0, 0, 1, 1]
+console.log(segregateZerosAndOnesType1([1, 1, 1, 0, 0])); // Output: [0, 0, 1, 1, 1]
+
+// type 2 : two pointer approach
+const segregateZerosAndOnesType2 = (arr) => {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        if (arr[left] === 0) {
+            left++;
+        }
+        else if (arr[right] === 1) {
+            right--;
+        }
+        else{
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+            left++;
+            right--;
+        }
+    }
+    return arr;
+};
+console.log(segregateZerosAndOnesType2([0, 1, 0, 1, 0])); // Output: [0, 0, 0, 1, 1]
+console.log(segregateZerosAndOnesType2([1, 1, 1, 0, 0])); // Output: [0, 0, 1, 1, 1]
